1232eeb6ee340081fb03073f6bd083545d75ff3d
Maurício Cordeiro <mauriciocordeiro@live.com>
2021-03-31 15:09:37 -0300

    identation

--- np-database/main.py
+++ np-database/main.py
@@ -21,12 +21,12 @@ db = client[os.environ['MONGODB_DATABASE']]
 def add_vehicle():
 	_json = request.json
 	_plate = _json['plate']
-    _brand = _json['brand']
-    _model = _json['model']
+	_brand = _json['brand']
+	_model = _json['model']
 	_owner = _json['owner']
 	_address = _json['address']
-    _phone = _json['phone']
-    _allowed = _json['allowed']
+	_phone = _json['phone']
+	_allowed = _json['allowed']
 
 	if db.vehicle.find_one({'plate': _plate}) != None:
 		return bad_request('Vehicle already exists')
@@ -43,7 +43,7 @@ def add_vehicle():
             'phone': _phone,
             'allowed': _allowed
         })
-		log(_email, 'Vehicle CREATED')
+
 		resp = jsonify({
             '_id': str(id), 
             'plate': _plate, 
@@ -59,8 +59,6 @@ def add_vehicle():
 	else:
 		return not_found()
 
-		
-		
 
 @app.route('/vehicles', methods=['GET'])
 def vehicles():
@@ -83,28 +81,28 @@ def update_vehicle(id):
 	_json = request.json
 	_id = id
 	_plate = _json['plate']
-    _brand = _json['brand']
-    _model = _json['model']
+	_brand = _json['brand']
+	_model = _json['model']
 	_owner = _json['owner']
 	_address = _json['address']
-    _phone = _json['phone']
-    _allowed = _json['allowed']	
+	_phone = _json['phone']
+	_allowed = _json['allowed']	
 
 	# validate the received values
 	if _plate and _phone and _id and request.method == 'PUT':
 		# save edits
 		db.vehicle.update_one(
-            {'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, 
-            {'$set': {
-                'plate': _plate, 
-                'brand': _brand, 
-                'model': _model, 
-                'owner': _owner, 
-                'address': _address, 
-                'phone': _phone,
-                'allowed': _allowed
-                }
-            })
+			{'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, 
+			{'$set': {
+				'plate': _plate, 
+				'brand': _brand, 
+				'model': _model, 
+				'owner': _owner, 
+				'address': _address, 
+				'phone': _phone,
+				'allowed': _allowed
+				}
+			})
 		resp = jsonify('Vehicle updated successfully!')
 		resp.status_code = 200
 		return resp
@@ -120,47 +118,57 @@ def delete_vehicle(id):
 	resp = jsonify('Vehicle deleted successfully!')
 	resp.status_code = 200
 
-	log(vehicle.email, 'Vehicle DELETED')
+	return resp
 
+@app.route('/', methods=['GET'])
+def index():
+	message = {
+		'status': 200,
+		'message': 'OK',
+		'detail': 'Seems fine'
+	}
+
+	resp = jsonify(message)
+	resp.status_code = 200
 	return resp
 
 
 @app.errorhandler(404)
 def not_found(error=None):
-    message = {
-        'status': 404,
-        'message': 'Not Found: ' + request.url,
-    }
-    resp = jsonify(message)
-    resp.status_code = 404
+	message = {
+		'status': 404,
+		'message': 'Not Found: ' + request.url,
+	}
+	resp = jsonify(message)
+	resp.status_code = 404
 
-    return resp
+	return resp
 
 
 @app.errorhandler(400)
 def bad_request(error=None):
-    message = {
-        'status': 400,
-        'message': error,
+	message = {
+		'status': 400,
+		'message': error,
 		'detail': 'Bad Request'
-    }
-    resp = jsonify(message)
-    resp.status_code = 400
+	}
+	resp = jsonify(message)
+	resp.status_code = 400
 
-    return resp
+	return resp
 
 
 @app.errorhandler(500)
 def nternal_server_error(error=None):
-    message = {
-        'status': 500,
-        'message': 'Internal Server Error' + request.url,
-    }
-    resp = jsonify(message)
-    resp.status_code = 500
+	message = {
+		'status': 500,
+		'message': 'Internal Server Error' + request.url,
+	}
+	resp = jsonify(message)
+	resp.status_code = 500
 
-    return resp
+	return resp
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
