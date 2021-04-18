9ca3e664bbbf1f64d8bd22c9c2a0fb86abe2fef3
Maurício Cordeiro <mauriciocordeiro@live.com>
2021-04-18 17:56:03 -0300

    bugs

--- checkts/src/index.ts
+++ checkts/src/index.ts
@@ -25,7 +25,7 @@ const app = express();
  */
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 app.use("/vehicles/check", vehicleRoute);
 
 /**
--- checkts/src/routes/vehicle.route.ts
+++ checkts/src/routes/vehicle.route.ts
@@ -5,6 +5,9 @@ import express, { Request, Response } from "express";
 import { Vehicle } from "../model/vehicle.model";
 import * as VehicleService from "../service/vehicle.service";
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 /**
  * Router Definition
  */
@@ -13,10 +16,13 @@ export const vehicleRoute = express.Router();
 /**
  * Controller Definitions
  */
-vehicleRoute.post("/", async (req: Request, res: Response) => {
+vehicleRoute.post("/", upload.single('img'), async (req: Request, res: Response) => {
     try {
-        console.log('doing things...')
-        const vehicle:Vehicle = await VehicleService.check(req.body);
+
+        console.log('body: ', req.body)
+        console.log('file: ', req.file)
+
+        const vehicle:Vehicle = await VehicleService.check(req.file);
         res.status(200).json(vehicle);
     } catch (e) {
         console.error(e);
